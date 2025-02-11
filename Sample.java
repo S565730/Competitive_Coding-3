173019b740e4b2e90983b15c26b4cf81b07bb02b
Rachana <S565730@nwmissouri.edu>
2025-02-11 13:40:10 -0500

    done

--- Sample.java
+++ Sample.java
@@ -1,7 +1,12 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//problem 1
+// Time Complexity :o(n^2)
+// Space Complexity :o(1)
+// Did this code successfully run on Leetcode :no
+// Any problem you faced while coding this :no
 
-
+//problem 2
+// Time Complexity :o(n)
+// Space Complexity :o(n)
+// Did this code successfully run on Leetcode :no
+// Any problem you faced while coding this :no
 // Your code here along with comments explaining your approach
